22a9a8667a486b8cca26b1a68a3d8fe8719083c7
Brian Casey <bjc17b@acu19466.lan>
2021-07-11 18:15:04 -0500

    Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
+
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
+import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     "extensions": [".svelte", ...mdsvexConfig.extensions],
 
     kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+        adapter: adapter(),
 		target: '#svelte'
 	},
 
